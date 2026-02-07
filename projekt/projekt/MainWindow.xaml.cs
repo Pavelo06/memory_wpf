12b14a4825b3a1626dba348c60248e4297b1e3b5
Pavelo06 <62283901+Pavelo06@users.noreply.github.com>
2026-02-07 21:24:45 +0100

    dodano zdjęcia i wstępną logikę do jednego przycisku

--- projekt/projekt/MainWindow.xaml.cs
+++ projekt/projekt/MainWindow.xaml.cs
@@ -19,6 +19,40 @@ namespace projekt
         public MainWindow()
         {
             InitializeComponent();
+            PrzypisanieUidImgKarty();
+        }
+
+        Random rand = new Random();
+
+        //zrobić całą logikę odkrywania kart i zakrywania z powrotem przy nieodgadnięciu,
+        //naliczanie puntków itd.
+
+        private void PrzypisanieUidImgKarty()
+        {
+            //lista zawierająca po dwie takie same wartości dla kart, po wybraniu usuwa jeden element
+            //z tej tablicy
+            List<int> list = new List<int>{ 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
+
+            int r = rand.Next(0, list.Count+1);
+
+            imgKarta1.Uid = list[r].ToString();
+            list.Remove(r);
+            //for (int i = 0; i <= tab.Length; i++) rozwazyć zrobienie tego pętlą
+            //{
+            //}
+        }
+
+        private void ClickOdkryjKarte(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            
+
+            imgKarta1.Source = new BitmapImage(new Uri(@$"img/card_{imgKarta1.Uid}.png", UriKind.Relative));
+            
+
+
+
+            btn.IsEnabled = false;
         }
     }
 }
